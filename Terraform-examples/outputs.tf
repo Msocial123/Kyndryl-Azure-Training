c75f4315050e370ef6a7cc5a95480e0f25790af8
MuraliMohan <muralisocial123@gmail.com>
2026-02-10 11:19:17 +0530

    Committed

---
+++ Terraform-examples/outputs.tf
@@ -0,0 +1,12 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.this.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.this.kube_config_raw
+  sensitive = true
+}
